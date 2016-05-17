c80b47d92aac71460446ef2ff621758444ee2c8b
Mario <mcarrillo.05@hotmail.com>
2016-05-16 20:14:03 -0500

    ¬¬

--- bot.go
+++ bot.go
@@ -3,16 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/frodsan/fbot"
 )
 
 func main() {
 	bot := fbot.NewBot(fbot.Config{
-		AccessToken: os.Getenv("EAALVTDYz2rcBACQkrtrSon8eNXZCtJl40HA1bSwfKpOMV977JFsB7EXuD3hrYxnugW9s837KSxe5w6JscQSwMsbjPZA62ZBIqyyAl2ArsmDlJtxnfEGREvffwJBsefbxs24jyPJTyhdYLvmC65CU7YT8jtyoJMeEL4lqmlGXQZDZD"),
-		AppSecret:   os.Getenv("2032fab87b7b8197b430cf5a65c9b639"),
-		VerifyToken: os.Getenv("0b8baf8b94e97c2416584afc2a8e9016"),
+		AccessToken: "EAALVTDYz2rcBACQkrtrSon8eNXZCtJl40HA1bSwfKpOMV977JFsB7EXuD3hrYxnugW9s837KSxe5w6JscQSwMsbjPZA62ZBIqyyAl2ArsmDlJtxnfEGREvffwJBsefbxs24jyPJTyhdYLvmC65CU7YT8jtyoJMeEL4lqmlGXQZDZD",
+		AppSecret:   "2032fab87b7b8197b430cf5a65c9b639",
+		VerifyToken: "0b8baf8b94e97c2416584afc2a8e9016",
 	})
 
 	bot.On(fbot.EventMessage, func(event *fbot.Event) {
