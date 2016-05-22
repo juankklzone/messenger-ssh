5137a518947d5d5d1e7a8d943a4d428e0edc6b18
Juan Carlos García <juan@alepht.com>
2016-05-22 13:54:33 -0500

    debuggin' TeeReader

--- src/mssh/client.go
+++ src/mssh/client.go
@@ -63,7 +63,7 @@ func startSession(m Messaging) (err error) {
 	}
 	u.session.Stdout = u.readBuf
 	u.session.Stdin = u.writeBuf
-	io.TeeReader(u.session.Stdin, u.writeBuf)
+	tee := io.TeeReader(u.session.Stdin, u.writeBuf)
 	modes := ssh.TerminalModes{
 		ssh.ECHO:          0,     // disable echoing
 		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
@@ -78,6 +78,14 @@ func startSession(m Messaging) (err error) {
 	}
 	mapaUsuarios[u.id] = u
 	go func() { u.session.Wait() }()
+	leerDatos := func(r io.Reader) {
+		b, err := ioutil.ReadAll(r)
+		if err != nil {
+			fmt.Println("error al leer tee", err)
+		}
+		fmt.Printf("%s", b)
+	}
+	go leerDatos(tee)
 	return
 }
 
